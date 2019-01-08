164d89fb9efefd8f8454057c11cf1f1ca76d4427
ShuheiIwamoto <shuhei.iwamoto.work@gmail.com>
2019-01-09 02:13:22 +0900

    no message

---
+++ TestUnityPackageManager/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace TestUnityPackageManager
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Checks if a string is null or empty.
+		/// </summary>
+		/// <param name="str">the string to check, may be null.</param>
+		/// <returns>if the string is empty or null.</returns>
+		public static bool IsNullOrEmpty(this string str) {
+			return string.IsNullOrEmpty(str);
+		}
+	}
+}
